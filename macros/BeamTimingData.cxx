4bfff66594818cf5098e00c74c8e84da41bb8cab
mlawe <m.lawe@lancaster.ac.uk>
2017-06-26 11:26:06 +0100

    Add legend plotting macro and update Beam Timing plotting macros to do seperate RMMs and all together.

--- macros/BeamTimingData.cxx
+++ macros/BeamTimingData.cxx
@@ -1,7 +1,7 @@
 #include "BeamTimingData.hxx"
 
 BeamTimingData::BeamTimingData(std::string fileList) {
-
+  
   std::ifstream inputFile(fileList.c_str(), std::ios::in);
   if (!inputFile){
     std::cerr << "Can not open input file: " << fileList << std::endl;
@@ -9,7 +9,7 @@ BeamTimingData::BeamTimingData(std::string fileList) {
   }
 
   std::string inputString;
-
+  
   while (inputFile >> inputString){
     std::cout << "Adding file: " << inputString << std::endl;
     if(exists_file(inputString.c_str()))
@@ -26,24 +26,32 @@ BeamTimingData::BeamTimingData(std::string fileList) {
   inputString = fRootFiles.at(0)->GetName();
 
   if (inputString.find("ECAL") != std::string::npos)
-    {fDet = "ECAL"; std::copy(minTimeBunchECAL, minTimeBunchECAL+8, minTimeBunch);}
+    {fDet = "ECal"; std::copy(minTimeBunchECAL, minTimeBunchECAL+8, minTimeBunch); fnRMMs = nRMM_ECAL;}
   else if (inputString.find("P0D") != std::string::npos)
-    {fDet = "P0D";  std::copy(minTimeBunchP0D, minTimeBunchP0D+8, minTimeBunch);}
+    {fDet = "P0D";  std::copy(minTimeBunchP0D, minTimeBunchP0D+8, minTimeBunch); fnRMMs = nRMM_P0D;}
   else if (inputString.find("SMRD") != std::string::npos)
-    {fDet = "SMRD"; std::copy(minTimeBunchSMRD, minTimeBunchSMRD+8, minTimeBunch);}
+    {fDet = "SMRD"; std::copy(minTimeBunchSMRD, minTimeBunchSMRD+8, minTimeBunch); fnRMMs = nRMM_SMRD;}
   else {
     std::cout << "Detector name not found in input file" << std::endl;
     return;
   }
   
+
+  // Do All, then loop over RMMs
   FillGraphs();
   SetGraphStyle();
   DrawGraphs();
+
+  for(int rmm = 0; rmm < fnRMMs; rmm++){
+    FillGraphs(rmm);
+    SetGraphStyle();
+    DrawGraphs(rmm);
+  }
   
   return;
 }
 
-void BeamTimingData::FillGraphs() {
+void BeamTimingData::FillGraphs(int rmm) {
   
   TGraphErrors *fileHistoMean;    // define all the graphs for each file and each bunch
   TGraphErrors *fileHistoSigma;   // define all the graphs for each file and each bunch
@@ -56,13 +64,23 @@ void BeamTimingData::FillGraphs() {
   // Loop over the bunches
   for(int iBunch = 0; iBunch < nBunch; iBunch++){
     OutOfRange[iBunch] = 0;
-
+    
     fHistoMean [iBunch] = new TGraphErrors();
     fHistoSigma[iBunch] = new TGraphErrors();
     if (iBunch) fHistoSep[iBunch-1] = new TGraphErrors(); // Don't create for first bunch
     
-    char *NameMean  = Form("timingfit_rmm_all_b%d_Mean",  iBunch+1);
-    char *NameSigma = Form("timingfit_rmm_all_b%d_Sigma", iBunch+1);
+    char *NameMean;
+    char *NameSigma; 
+
+    if(rmm!=-999) {
+      NameMean  = Form("timingfit_rmm%02d_b%d_Mean",  rmm, iBunch+1);
+      NameSigma = Form("timingfit_rmm%02d_b%d_Sigma", rmm, iBunch+1);
+    }
+    else {
+      NameMean  = Form("timingfit_rmm_all_b%d_Mean",  iBunch+1);
+      NameSigma = Form("timingfit_rmm_all_b%d_Sigma", iBunch+1);
+    }
+
     TKey *keyMean  = fRootFiles.at(0)->FindKey(NameMean );
     TKey *keySigma = fRootFiles.at(0)->FindKey(NameSigma);
     
@@ -181,9 +199,6 @@ void BeamTimingData::SetGraphStyleMean() {
     fHistoMean[iBunch]->SetLineWidth(0.35);
     // fHistoMean[iBunch]->SetLineColor(iBunch%4+2);
     fHistoMean[iBunch]->SetLineColor( (iBunch<4 ? iBunch+1 : iBunch+2) );
-    
-    // fHistoMean[iBunch]->SetMinimum(0.9*fMinSigma);
-    // fHistoMean[iBunch]->SetMaximum(1.1*fMaxSigma);
   }
 
   return;
@@ -229,8 +244,8 @@ void BeamTimingData::SetGraphStyleSigma() {
     // fHistoSigma[iBunch]->SetLineColor(iBunch%4+2);
     fHistoSigma[iBunch]->SetLineColor( (iBunch<4 ? iBunch+1 : iBunch+2) );
     
-    fHistoSigma[iBunch]->SetMinimum(0.9*fMinSigma);
-    fHistoSigma[iBunch]->SetMaximum(1.1*fMaxSigma);
+    fHistoSigma[iBunch]->SetMinimum(fMinSigma-(0.1*(fMaxSigma-fMinSigma)));
+    fHistoSigma[iBunch]->SetMaximum(fMaxSigma+(0.1*(fMaxSigma-fMinSigma)));
   }
 
   return;
@@ -274,14 +289,14 @@ void BeamTimingData::SetGraphStyleSep() {
     fHistoSep[iBunch]->SetLineWidth(0.35);
     fHistoSep[iBunch]->SetLineColor( (iBunch<3 ? iBunch+2 : iBunch+3) );
     
-    fHistoSep[iBunch]->SetMinimum(0.9*fMinSep);
-    fHistoSep[iBunch]->SetMaximum(1.1*fMaxSep);
+    fHistoSep[iBunch]->SetMinimum(fMinSep-(0.1*(fMaxSep-fMinSep)));
+    fHistoSep[iBunch]->SetMaximum(fMaxSep+(0.1*(fMaxSep-fMinSep)));
   }
 
   return;
 }
 
-void BeamTimingData::DrawGraphMean(){
+void BeamTimingData::DrawGraphMean(int rmm){
 
   TCanvas* can = new TCanvas("can_mean",Form("%s Bunch Timing", fDet.c_str()), 600, 600);
   can->SetTitle("");
@@ -339,111 +354,109 @@ void BeamTimingData::DrawGraphMean(){
     
   } // for(int iBunch = 0...
   
+  std::vector<TPad*> labelMask;
+  // Label masks for ECal
+  if (fDet.find("ECal") != std::string::npos) {
+    labelMask.push_back(new TPad("","",0.0,0.705,0.105,0.745));
+    labelMask.push_back(new TPad("","",0.0,0.605,0.105,0.645));
+    labelMask.push_back(new TPad("","",0.0,0.505,0.105,0.550));  
+    labelMask.push_back(new TPad("","",0.0,0.390,0.105,0.420));
+    labelMask.push_back(new TPad("","",0.0,0.280,0.105,0.350));
+    labelMask.push_back(new TPad("","",0.0,0.190,0.105,0.225));
+  }
+
+  // Label masks for P0D
+  if (fDet.find("P0D") != std::string::npos) {
+    labelMask.push_back(new TPad("","",0.0,0.705,0.105,0.745));
+    labelMask.push_back(new TPad("","",0.0,0.605,0.105,0.645));
+    labelMask.push_back(new TPad("","",0.0,0.505,0.105,0.550));  
+    labelMask.push_back(new TPad("","",0.0,0.390,0.105,0.420));
+    labelMask.push_back(new TPad("","",0.0,0.280,0.105,0.345));
+    labelMask.push_back(new TPad("","",0.0,0.190,0.105,0.225));
+  }
+
+  // Label masks for SMRD
+  if (fDet.find("SMRD") != std::string::npos) {
+    labelMask.push_back(new TPad("","",0.0,0.810,0.105,0.841));
+    labelMask.push_back(new TPad("","",0.0,0.710,0.105,0.745));
+    labelMask.push_back(new TPad("","",0.0,0.500,0.105,0.535));  
+    labelMask.push_back(new TPad("","",0.0,0.400,0.105,0.430));
+    labelMask.push_back(new TPad("","",0.0,0.185,0.105,0.220));
+  }
+  
+  for(std::vector<TPad*>::iterator it = labelMask.begin(); it != labelMask.end(); ++it)
+    (*it)->Draw();
+
   TPad *maskPads[nBunch-1];
   TLine *vertLine = new TLine(0.991,0,0.991,1.00); vertLine->SetLineWidth(0.1); vertLine->SetLineColor(1);
-  TLine *cutLine1 = new TLine(0.068,0,0.068,0.3);  cutLine1->SetLineWidth(0.1); cutLine1->SetLineColor(1);
-  TLine *cutLine2 = new TLine(0.04,0.1,0.096,0.5); cutLine2->SetLineWidth(0.1); cutLine2->SetLineColor(1);
-  TLine *cutLine3 = new TLine(0.04,0.5,0.096,0.9); cutLine3->SetLineWidth(0.1); cutLine3->SetLineColor(1);
-  TLine *cutLine4 = new TLine(0.068,0.7,0.068,1);  cutLine4->SetLineWidth(0.1); cutLine4->SetLineColor(1); 
+
+  std::vector<TLine*> cutLine;
+  cutLine.push_back(new TLine(0.068,0,0.068,0.3));
+  cutLine.push_back(new TLine(0.04,0.1,0.096,0.5));
+  cutLine.push_back(new TLine(0.04,0.5,0.096,0.9));
+  cutLine.push_back(new TLine(0.068,0.7,0.068,1));
+  
+  for(std::vector<TLine*>::iterator it = cutLine.begin(); it != cutLine.end(); ++it) {
+    (*it)->SetLineWidth(0.1);
+    (*it)->SetLineColor(1);
+  }
   
   ypadhigh = 0.204;
-
-  // Set for only ECal
-  TPad *labelMask1 = new TPad("","",0.0,0.705,0.105,0.745); labelMask1->Draw();
-  TPad *labelMask2 = new TPad("","",0.0,0.605,0.105,0.645); labelMask2->Draw();
-  TPad *labelMask3 = new TPad("","",0.0,0.505,0.105,0.550); labelMask3->Draw();  
-  TPad *labelMask4 = new TPad("","",0.0,0.390,0.105,0.420); labelMask4->Draw();
-  TPad *labelMask5 = new TPad("","",0.0,0.280,0.105,0.350); labelMask5->Draw();
-  TPad *labelMask6 = new TPad("","",0.0,0.190,0.105,0.225); labelMask6->Draw();
-
+  
   for(int iBunch = 0; iBunch < nBunch-1; iBunch++){
     maskPads[iBunch] = new TPad("","",0.05,ypadhigh-0.006,0.9,ypadhigh+0.007);
     maskPads[iBunch]->Draw();
     maskPads[iBunch]->cd();
     
     vertLine->Draw();
-    cutLine1->Draw(); cutLine2->Draw(); cutLine3->Draw(); cutLine4->Draw();
-
+    for(std::vector<TLine*>::iterator it = cutLine.begin(); it != cutLine.end(); ++it)
+      (*it)->Draw();
+    
     ypadhigh+=ypadsep;
     can->cd();
   }
-
-  // // Draw a nice Y axis
-  // TPad *p87 = new TPad("p87","p87",0.05,0.825,0.9,0.850);p87->Draw();p87->cd();
-  // TLine *l87 = new TLine(0.991,0,0.991,0.99); l87->SetLineWidth(0.1); l87->SetLineColor(1); l87->Draw();
-  // TLine *cut87_1 = new TLine(0.068,0,0.068,0.3); cut87_1->SetLineWidth(0.1); cut87_1->SetLineColor(1); cut87_1->Draw();
-  // TLine *cut87_2 = new TLine(0.04,0.1,0.096,0.5); cut87_2->SetLineWidth(0.1); cut87_2->SetLineColor(1); cut87_2->Draw();
-  // TLine *cut87_3 = new TLine(0.04,0.5,0.096,0.9); cut87_3->SetLineWidth(0.1); cut87_3->SetLineColor(1); cut87_3->Draw();
-  // TLine *cut87_4 = new TLine(0.068,0.7,0.068,1); cut87_4->SetLineWidth(0.1); cut87_4->SetLineColor(1); cut87_4->Draw();
-  
-  // can->cd();
-  // TPad *p76 = new TPad("p76","p76",0.0,0.710,0.9,0.745);p76->Draw();p76->cd();
-  // TLine *l76 = new TLine(0.991,0,0.991,0.99); l76->SetLineWidth(0.1); l76->SetLineColor(1); l76->Draw();
-  // TLine *cut76_1 = new TLine(0.12,0,0.12,0.3); cut76_1->SetLineWidth(0.1); cut76_1->SetLineColor(1); cut76_1->Draw();
-  // TLine *cut76_2 = new TLine(0.09,0.1,0.146,0.5); cut76_2->SetLineWidth(0.1); cut76_2->SetLineColor(1); cut76_2->Draw();
-  // TLine *cut76_3 = new TLine(0.09,0.5,0.146,0.9); cut76_3->SetLineWidth(0.1); cut76_3->SetLineColor(1); cut76_3->Draw();
-  // TLine *cut76_4 = new TLine(0.12,0.7,0.12,1); cut76_4->SetLineWidth(0.1); cut76_4->SetLineColor(1); cut76_4->Draw();
-  
-  // can->cd();
-  // TPad *p65label_mask = new TPad("p65lm","p65lm",0.,0.605,0.105,0.645);p65label_mask->Draw();
-  // TPad *p65 = new TPad("p65","p65",0.05,0.613,0.9,0.638);p65->Draw();p65->cd();
-  // TLine *l65 = new TLine(0.991,0,0.991,0.99); l65->SetLineWidth(0.1); l65->SetLineColor(1); l65->Draw();
-  // TLine *cut65_1 = new TLine(0.068,0,0.068,0.3); cut65_1->SetLineWidth(0.1); cut65_1->SetLineColor(1); cut65_1->Draw();
-  // TLine *cut65_2 = new TLine(0.04,0.1,0.096,0.5); cut65_2->SetLineWidth(0.1); cut65_2->SetLineColor(1); cut65_2->Draw();
-  // TLine *cut65_3 = new TLine(0.04,0.5,0.096,0.9); cut65_3->SetLineWidth(0.1); cut65_3->SetLineColor(1); cut65_3->Draw();
-  // TLine *cut65_4 = new TLine(0.068,0.7,0.068,1); cut65_4->SetLineWidth(0.1); cut65_4->SetLineColor(1); cut65_4->Draw();
-  
-  // can->cd();
-  // TPad *p54label_mask = new TPad("p54lm","p54lm",0.,0.505,0.105,0.55);p54label_mask->Draw();
-  // TPad *p54 = new TPad("p54","p54",0.05,0.508,0.9,0.533);p54->Draw();p54->cd();
-  // TLine *l54 = new TLine(0.991,0,0.991,1); l54->SetLineWidth(0.1); l54->SetLineColor(1); l54->Draw();
-  // TLine *cut54_1 = new TLine(0.068,0,0.068,0.3);  cut54_1->SetLineWidth(0.1); cut54_1->SetLineColor(1); cut54_1->Draw();
-  // TLine *cut54_2 = new TLine(0.04,0.1,0.096,0.5); cut54_2->SetLineWidth(0.1); cut54_2->SetLineColor(1); cut54_2->Draw();
-  // TLine *cut54_3 = new TLine(0.04,0.5,0.096,0.9); cut54_3->SetLineWidth(0.1); cut54_3->SetLineColor(1); cut54_3->Draw();
-  // TLine *cut54_4 = new TLine(0.068,0.7,0.068,1);  cut54_4->SetLineWidth(0.1); cut54_4->SetLineColor(1); cut54_4->Draw();
-  
-  // can->cd();
-  // TPad *p43label_mask = new TPad("p43lm","p43lm",0.,0.39,0.105,0.43);p43label_mask->Draw();
-  // TPad *p43 = new TPad("p43","p43",0.05,0.403,0.9,0.428);p43->Draw();p43->cd();
-  // TLine *l43 = new TLine(0.991,0,0.991,0.99);l43->SetLineWidth(0.1);l43->SetLineColor(1);l43->Draw();
-  // TLine *cut43_1 = new TLine(0.068,0,0.068,0.3);  cut43_1->SetLineWidth(0.1); cut43_1->SetLineColor(1); cut43_1->Draw();
-  // TLine *cut43_2 = new TLine(0.04,0.1,0.096,0.5); cut43_2->SetLineWidth(0.1); cut43_2->SetLineColor(1); cut43_2->Draw();
-  // TLine *cut43_3 = new TLine(0.04,0.5,0.096,0.9); cut43_3->SetLineWidth(0.1); cut43_3->SetLineColor(1); cut43_3->Draw();
-  // TLine *cut43_4 = new TLine(0.068,0.7,0.068,1);  cut43_4->SetLineWidth(0.1); cut43_4->SetLineColor(1); cut43_4->Draw();
-  
-  // can->cd();
-  // TPad *p32label_mask = new TPad("p32lm","p32lm",0.0,0.280,0.105,0.35);p32label_mask->Draw();
-  // TPad *p32 = new TPad("p32","p32",0.05,0.297,0.9,0.322);p32->Draw();p32->cd();
-  // TLine *l32 = new TLine(0.991,0,0.991,0.99); l32->SetLineWidth(0.1); l32->SetLineColor(1); l32->Draw();
-  // TLine *cut32_1 = new TLine(0.068,0,0.068,0.3);  cut32_1->SetLineWidth(0.1); cut32_1->SetLineColor(1); cut32_1->Draw();
-  // TLine *cut32_2 = new TLine(0.04,0.1,0.096,0.5); cut32_2->SetLineWidth(0.1); cut32_2->SetLineColor(1); cut32_2->Draw();
-  // TLine *cut32_3 = new TLine(0.04,0.5,0.096,0.9); cut32_3->SetLineWidth(0.1); cut32_3->SetLineColor(1); cut32_3->Draw();
-  // TLine *cut32_4 = new TLine(0.068,0.7,0.068,1);  cut32_4->SetLineWidth(0.1); cut32_4->SetLineColor(1); cut32_4->Draw();
-  
-  //   can->cd();
-  // TPad *p21label_mask = new TPad("p21lm","p21lm",0.0,0.18,0.105,0.225);p21label_mask->Draw();
-  // TPad *p21 = new TPad("p21","p21",0.05,0.185,0.9,0.217);p21->Draw();p21->cd();
-  // TLine *l21 = new TLine(0.991,0,0.991,0.99); l21->SetLineWidth(0.1); l21->SetLineColor(1); l21->Draw();
-  // TLine *cut21_1 = new TLine(0.068,0,0.068,0.3);  cut21_1->SetLineWidth(0.1); cut21_1->SetLineColor(1); cut21_1->Draw();
-  // TLine *cut21_2 = new TLine(0.04,0.1,0.096,0.5); cut21_2->SetLineWidth(0.1); cut21_2->SetLineColor(1); cut21_2->Draw();
-  // TLine *cut21_3 = new TLine(0.04,0.5,0.096,0.9); cut21_3->SetLineWidth(0.1); cut21_3->SetLineColor(1); cut21_3->Draw();
-  // TLine *cut21_4 = new TLine(0.068,0.7,0.068,1);  cut21_4->SetLineWidth(0.1); cut21_4->SetLineColor(1); cut21_4->Draw();
-  // can->cd();
-  
   
   // Print the title
-  TText *titleboxsigma = new TText(0.01,0.96, Form("%s Bunch Timing [ns]", fDet.c_str())); // 0.01 0.96
+  TText *titleboxsigma;
+  if(rmm!=-999)
+    titleboxsigma = new TText(0.01,0.96, Form("%s RMM %02d Bunch Timing [ns]", fDet.c_str(), rmm)); // 0.01 0.96
+  else
+    titleboxsigma = new TText(0.01,0.96, Form("%s Bunch Timing [ns]", fDet.c_str(), rmm)); // 0.01 0.96
+
   titleboxsigma->SetNDC();
   titleboxsigma->SetTextSize(0.055); // 0.055
   titleboxsigma->Draw();
 
-
-  can->SaveAs(Form("%s_bunchtiming_weekly_all.png", fDet.c_str()));
+  if(rmm!=-999)
+    can->SaveAs(Form("%s_bunchtiming_weekly_rmm%02d.png", fDet.c_str(), rmm));
+  else
+    can->SaveAs(Form("%s_bunchtiming_weekly_all.png", fDet.c_str()));
   
+  // clean up
+  for(int iBunch = 0; iBunch < nBunch; iBunch++){
+    if (iBunch) delete maskPads[iBunch-1];
+    delete textbox[iBunch];
+    delete lineFits[iBunch];
+    delete pad[iBunch];
+    delete fHistoMean[iBunch];
+  }
+
+  for(std::vector<TLine*>::iterator it = cutLine.begin(); it != cutLine.end(); ++it)
+    delete *it;
+  cutLine.clear();
+
+  for(std::vector<TPad*>::iterator it = labelMask.begin(); it != labelMask.end(); ++it)
+    delete *it;
+  labelMask.clear();
+
+  delete titleboxsigma;
+  delete vertLine;
+  delete can;
+
   return;
 }
 
-void BeamTimingData::DrawGraphSigma(){
+void BeamTimingData::DrawGraphSigma(int rmm){
 
   TCanvas* can = new TCanvas("can_sigma",Form("%s Bunch Width", fDet.c_str()), 600, 600);
   can->SetTitle("");
@@ -455,17 +468,32 @@ void BeamTimingData::DrawGraphSigma(){
   }
 
   // Print the title
-  TText *titleboxsigma = new TText(0.01,0.96, Form("%s Bunch Width [ns]", fDet.c_str())); // 0.01 0.96
+  TText *titleboxsigma;
+  if(rmm!=-999)
+    titleboxsigma = new TText(0.01,0.96, Form("%s RMM %02d Bunch Width [ns]", fDet.c_str(), rmm)); // 0.01 0.96
+  else
+    titleboxsigma = new TText(0.01,0.96, Form("%s Bunch Width [ns]", fDet.c_str(), rmm)); // 0.01 0.96
+  
   titleboxsigma->SetNDC();
   titleboxsigma->SetTextSize(0.055); // 0.055
   titleboxsigma->Draw();
   
-  can->SaveAs(Form("%s_bunchwidth_weekly_all.png", fDet.c_str()));
+  if(rmm!=-999)
+    can->SaveAs(Form("%s_bunchwidth_weekly_rmm%02d.png", fDet.c_str(), rmm));
+  else
+    can->SaveAs(Form("%s_bunchwidth_weekly_all.png", fDet.c_str()));
   
+  // clean up
+  for(int iBunch = 0; iBunch < nBunch; iBunch++)
+    delete fHistoSigma[iBunch];
+    
+  delete titleboxsigma;
+  delete can;
+
   return;
 }
 
-void BeamTimingData::DrawGraphSep(){
+void BeamTimingData::DrawGraphSep(int rmm){
 
   TCanvas* can = new TCanvas("can_sep",Form("%s Bunch Separation", fDet.c_str()), 600, 600);
   can->SetTitle("");
@@ -477,31 +505,40 @@ void BeamTimingData::DrawGraphSep(){
   }
 
   // Print the title
-  TText *titleboxsigma = new TText(0.01,0.96, Form("%s Bunch Separation [ns]", fDet.c_str())); // 0.01 0.96
+  TText *titleboxsigma;
+  if(rmm!=-999)
+    titleboxsigma = new TText(0.01,0.96, Form("%s RMM %02d Bunch Separation [ns]", fDet.c_str(), rmm)); // 0.01 0.96
+  else
+    titleboxsigma = new TText(0.01,0.96, Form("%s Bunch Separation [ns]", fDet.c_str(), rmm)); // 0.01 0.96
+
   titleboxsigma->SetNDC();
   titleboxsigma->SetTextSize(0.055); // 0.055
   titleboxsigma->Draw();
+
+  if(rmm!=-999)
+    can->SaveAs(Form("%s_bunchseparation_weekly_rmm%02d.png", fDet.c_str(), rmm));
+  else
+    can->SaveAs(Form("%s_bunchseparation_weekly_all.png", fDet.c_str()));
   
-  can->SaveAs(Form("%s_bunchseparation_weekly_all.png", fDet.c_str()));
+  // clean up
+  for(int iBunch = 0; iBunch < nBunch-1; iBunch++)
+    delete fHistoSep[iBunch];
+
+  delete titleboxsigma;
+  delete can;
   
   return;
 }
-  
+
 int main(int argc, char* argv[]) {
   
   // Process the options.
   for (;;) {
-    int c = getopt(argc, argv, "e:p:s:");
+    int c = getopt(argc, argv, "f:");
     if (c<0) break;
     switch (c) {
-    case 'e': {
-      BeamTimingData ecal(optarg);
-      break; }
-    case 'p': {
-      BeamTimingData p0d(optarg);
-      break; }
-    case 's': {
-      BeamTimingData smrd(optarg);
+    case 'f': {
+      BeamTimingData run(optarg);
       break; }
     }
   } // Closes process options for loop  
--- macros/BeamTimingData.hxx
+++ macros/BeamTimingData.hxx
@@ -23,7 +23,9 @@ static const double TimeRange = 100;
 static const double minTimeBunchECAL[8] = {2775,3350,3950,4525,5100,5700,6275,6860};
 static const double minTimeBunchP0D [8] = {2725,3300,3900,4475,5050,5650,6225,6810};
 static const double minTimeBunchSMRD[8] = {2750,3325,3925,4500,5075,5675,6250,6835};
-
+static const int nRMM_ECAL = 12;
+static const int nRMM_P0D  = 6;
+static const int nRMM_SMRD = 4;
 
 inline bool exists_file(const std::string& name){
   struct stat buffer;
@@ -35,31 +37,29 @@ class BeamTimingData {
 public:
   BeamTimingData(std::string);
 
-  TGraphErrors** GetMeanGraph() {return fHistoMean; };
-  TGraphErrors** GetSigmaGraph(){return fHistoSigma;};
-  TGraphErrors** GetSepGraph()  {return fHistoSep;  };
-
 protected:
   
 private:
 
-  void SetGraphStyle(){SetGraphStyleMean(); SetGraphStyleSigma(); SetGraphStyleSep();};
-  void DrawGraphs()   {DrawGraphMean();     DrawGraphSigma();     DrawGraphSep();    };
+  void SetGraphStyle()            {SetGraphStyleMean(); SetGraphStyleSigma(); SetGraphStyleSep();};
+  void DrawGraphs(int rmm = -999) {DrawGraphMean(rmm);  DrawGraphSigma(rmm);  DrawGraphSep(rmm); };
   
   void SetGraphStyleMean();  // Set the bunch timing draw style
   void SetGraphStyleSigma(); // Set the bunch width draw style
   void SetGraphStyleSep();   // Set the bunch spearation draw style
 
-  void DrawGraphMean();  // Draw the bunch timing graph
-  void DrawGraphSigma(); // Draw the bunch width graph
-  void DrawGraphSep();   // Draw the bunch spearation graph
+  void DrawGraphMean(int rmm = -999);  // Draw the bunch timing graph
+  void DrawGraphSigma(int rmm = -999); // Draw the bunch width graph
+  void DrawGraphSep(int rmm = -999);   // Draw the bunch spearation graph
   
-  void FillGraphs(); // Fill the graphs
+  void FillGraphs(int rmm = -999); // Fill the graphs
   
   double minTimeBunch[nBunch]; // Bunch times
-  std::string fDet; // Detector
 
-  int OutOfRange[nBunch];
+  std::string fDet; // Detector
+  int fnRMMs; // Number of RMMs
+
+  int OutOfRange[nBunch]; // Number of out of range data fits
   
   std::vector<TFile*> fRootFiles; // Files read
 
@@ -71,6 +71,5 @@ private:
   double fMaxSigma; // Maximum Sigma
   double fMinSep;   // Minimum Separation
   double fMaxSep;   // Maximum Separation
-
 
 };
