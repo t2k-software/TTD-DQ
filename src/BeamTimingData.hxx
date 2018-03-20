ec5cf07c545632d28508d7a8a4bf340d01f12b89
Matthew Hogan <hoganman@gmail.com>
2018-03-20 12:17:21 +0900

    cosmetic changes to BeamTimingData

--- src/BeamTimingData.hxx
+++ src/BeamTimingData.hxx
@@ -11,16 +11,16 @@
 // Compile with g++ -o BeamTimingData.exe BeamTimingData.*xx `root-config --cflags --libs`
 
 // Bunch of global variables because I'm a bad person
-static const int nBunch = 8;
-static const double TimeRange = 100;
-static const double minTimeBunchECAL[8] = {2775,3350,3950,4525,5100,5700,6275,6860};
-static const double minTimeBunchP0D [8] = {2725,3300,3900,4475,5050,5650,6225,6810};
-static const double minTimeBunchSMRD[8] = {2750,3325,3925,4500,5075,5675,6250,6835};
-static const int nRMM_ECAL = 12;
-static const int nRMM_P0D  = 6;
-static const int nRMM_SMRD = 4;
-static const int bunchSeparationDesign = 581; // Design Bunch Separation 581 ns
-static const int bunchWidthDesign = 58; // Design Bunch Width 58 ns
+static const Int_t nBunch = 8;
+static const Double_t TimeRange = 100;
+static const Double_t minTimeBunchECAL[8] = {2775,3350,3950,4525,5100,5700,6275,6860};
+static const Double_t minTimeBunchP0D [8] = {2725,3300,3900,4475,5050,5650,6225,6810};
+static const Double_t minTimeBunchSMRD[8] = {2750,3325,3925,4500,5075,5675,6250,6835};
+static const Int_t nRMM_ECAL = 12;
+static const Int_t nRMM_P0D  = 6;
+static const Int_t nRMM_SMRD = 4;
+static const Int_t bunchSeparationDesign = 581; // Design Bunch Separation 581 ns
+static const Int_t bunchWidthDesign = 58; // Design Bunch Width 58 ns
 
 inline bool exists_file(const std::string& name){
   struct stat buffer;
@@ -37,24 +37,24 @@ protected:
 private:
 
   void SetGraphStyle()            {SetGraphStyleMean(); SetGraphStyleSigma(); SetGraphStyleSep();};
-  void DrawGraphs(int rmm = -999) {DrawGraphMean(rmm);  DrawGraphSigma(rmm);  DrawGraphSep(rmm); };
+  void DrawGraphs(Int_t rmm = -999) {DrawGraphMean(rmm);  DrawGraphSigma(rmm);  DrawGraphSep(rmm); };
   
   void SetGraphStyleMean();  // Set the bunch timing draw style
   void SetGraphStyleSigma(); // Set the bunch width draw style
   void SetGraphStyleSep();   // Set the bunch spearation draw style
 
-  void DrawGraphMean(int rmm = -999);  // Draw the bunch timing graph
-  void DrawGraphSigma(int rmm = -999); // Draw the bunch width graph
-  void DrawGraphSep(int rmm = -999);   // Draw the bunch spearation graph
+  void DrawGraphMean(Int_t rmm = -999);  // Draw the bunch timing graph
+  void DrawGraphSigma(Int_t rmm = -999); // Draw the bunch width graph
+  void DrawGraphSep(Int_t rmm = -999);   // Draw the bunch spearation graph
   
-  void FillGraphs(int rmm = -999); // Fill the graphs
+  void FillGraphs(Int_t rmm = -999); // Fill the graphs
   
-  double minTimeBunch[nBunch]; // Bunch times
+  Double_t minTimeBunch[nBunch]; // Bunch times
 
   std::string fDet; // Detector
-  int fnRMMs; // Number of RMMs
+  Int_t fnRMMs; // Number of RMMs
 
-  int OutOfRange[nBunch]; // Number of out of range data fits
+  Int_t OutOfRange[nBunch]; // Number of out of range data fits
   
   std::vector<TFile*> fRootFiles; // Files read
 
@@ -62,10 +62,10 @@ private:
   TGraphErrors *fHistoSigma[nBunch]; // Bunch width
   TGraphErrors *fHistoSep  [nBunch-1]; // Bunch separation
 
-  double fMinSigma; // Minimum Sigma
-  double fMaxSigma; // Maximum Sigma
-  double fMinSep;   // Minimum Separation
-  double fMaxSep;   // Maximum Separation
+  Double_t fMinSigma; // Minimum Sigma
+  Double_t fMaxSigma; // Maximum Sigma
+  Double_t fMinSep;   // Minimum Separation
+  Double_t fMaxSep;   // Maximum Separation
 
 };
 
