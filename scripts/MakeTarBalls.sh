58c12e745c8e1973aabfa95b7b1579e3cb52e67a
Matthew Hogan <hoganman@gmail.com>
2018-03-21 17:22:05 +0900

    fixed tar statement

--- scripts/MakeTarBalls.sh
+++ scripts/MakeTarBalls.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
+
+####################################################################
+# After processing, copy the beam timing, gain, and pedestal drifts
+# into tarballs to be given to the corresponding DQ experts
+#
+# AUTHOR: Matthew Hogan
+# EMAIL: hoganman@rams.colostate.edu
+####################################################################
+
 TAR=$(which tar)
+TARCREATE="$TAR cvf"
+TAROPTIONS="--null -T -"
+
 FIND=$(which find)
+ONEPERLINE="-print0"
 
 f=$(readlink -f $0)
 . ${f%/*}/dateutils.sh
@@ -37,8 +50,26 @@ done
 
 date_dir=$(dateDir $week)
 
+# create tarballs for each detector
 for det in ECAL P0D SMRD; do
-    OUTDIR=$CURDIR/RunPeriods/$date_dir
-    $FIND $OUTDIR -type f "*${det}*root" -print0 | tar cvf $OUTDIR/${det}DQ.tar --null -T -
+
+    # define options and outputs
+    SEARCHDIR=$CURDIR/RunPeriods/$date_dir
+    DETROOTFILES="-type f -iname \"*${det}*root\""
+    OUTDIR=$SEARCHDIR
+    TARBALL=$OUTDIR/${det}DQ.tar
+
+    # find & tar commands
+    FINDFILES="$FIND $SEARCHDIR $DETROOTFILES $ONEPERLINE"
+    MAKETARBALL="$TARCREATE $TARBALL $TAROPTIONS"
+
+    # pipe together
+    $FINDFILES | $MAKETARBALL
+
 done
-echo ${date_dir}
+
+echo "DONE making tarballs for DQ experts."
+echo "Submit the following: "
+for det in ECAL P0D SMRD; do
+    echo "$CURDIR/RunPeriods/$date_dir/${det}DQ.tar to the $det DQ expert"
+done
